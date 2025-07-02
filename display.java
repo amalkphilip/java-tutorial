e8dc24c0e7b78724103a8f50b4d132d51df841bf
Amal K Philip <amal17philip@gmail.com>
2025-07-02 21:12:39 +0530

    Update display.java

--- display.java
+++ display.java
@@ -1,5 +1,5 @@
 public class helloworld{
-  public static void main{
-    system.out print In("HEllo WORLD!")
+  public static void main(String[] args){
+    System.out.printIn("HEllo WORLD!");
       }
 }
