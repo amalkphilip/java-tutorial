3697a1b3663a4896fa2c7e3064587e9220e3f43a
Amal K Philip <amal17philip@gmail.com>
2025-07-23 10:15:34 +0530

    Create StudentDemo.java

---
+++ StudentDemo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Student{
+	String Name;
+	int Age;
+
+	public void DisplayDetails() {
+		System.out.println("The Name of the Student: "+Name);
+		System.out.println("The Age of the Student: "+Age);
+	}
+}
+
+public class StudentDemo {
+	public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+	        Student student = new Student();
+
+	        System.out.print("Enter name: ");
+	        student.Name = scanner.nextLine();
+
+	        System.out.print("Enter age: ");
+	        student.Age = scanner.nextInt();
+		student.DisplayDetails();
+	}
+}
