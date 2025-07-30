e04d752533dd478c548a687f6c8935bbcf330493
Amal K Philip <amal17philip@gmail.com>
2025-07-30 10:50:38 +0530

    Create Bookdemo.java

---
+++ Bookdemo.java
@@ -0,0 +1,35 @@
+class Book{
+	String title;
+	String author;
+	double price;
+	int remain;
+	double total;
+	
+	Book(String title,String author,double price,int remain){
+		this.title = title;
+		this.author = author;
+		this.price = price;
+		this.remain = remain;
+		this.total = remain*price;
+	}
+	
+	
+	public void displayDetails() {
+		System.out.println("Title of the Book :"+title);
+		System.out.println("Author of the Book :"+author);
+		System.out.println("Price for the Book :"+price+" INR");
+		System.out.println("Books remaining :"+remain);
+		System.out.println("Total price of the Book :"+total+" INR");
+	
+	}
+}
+
+public class BookDemo {
+	public static void main(String[] args) {
+		Book bo1 = new Book("Harry Potter","J K Rowling",1000,8);
+		Book bo2 = new Book("Wings Of Fire","Tui .T. Sutherland",1000,8);
+		bo1.displayDetails();
+		System.out.println(" ");
+		bo2.displayDetails();
+		}
+}
