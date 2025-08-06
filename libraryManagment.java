2ef48f22b54285792a0e31eee8cdb1c4d7fb8b00
Amal K Philip <amal17philip@gmail.com>
2025-08-06 14:53:57 +0530

    Update libraryManagment.java

--- libraryManagment.java
+++ libraryManagment.java
@@ -1,3 +1,5 @@
+package java_tutorial;
+
 class Library{
 	String title;
 	String author;
@@ -7,15 +9,26 @@ class Library{
 	Library(){
 		this.title="UNDECLARED";
 		this.author="UNDEFINED";
+		counter++;
+		this.bookID=counter;
 	}
 	Library(String title,String author){
 		this.title=title;
 		this.author=author;
 		counter++;
+		this.bookID=counter;
 	}
+	
+	public void displayInfo() {
+		System.out.println("TITLE OF THE BOOK :"+title);
+		System.out.println("AUTHOR OF THE BOOK  :"+author);
+		System.out.println("ID OF THE BOOK :"+bookID);
+		System.out.println(counter+"th BOOK");
+	}
+	
 }
 
 
-public class library {
+public class libraryManagement {
 
 }
