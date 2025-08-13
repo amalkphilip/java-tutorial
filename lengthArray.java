46f72cf34657cd6108bb9898fe15e9329b2f147a
Amal K Philip <amal17philip@gmail.com>
2025-08-13 09:33:33 +0530

    Create lengthArray.java

---
+++ lengthArray.java
@@ -0,0 +1,12 @@
+public class lengthArray {
+	public static void main(String[] args) {
+		int[] anArray = {10,20,30,40,50};
+		System.out.println("Length of the ARRAY : "+anArray.length);
+		System.out.println("Values inside the Array:\n");
+		for(int i=0;i<anArray.length;i++) {
+			System.out.print(anArray[i]+"\t");
+		}
+		
+	}
+
+}
